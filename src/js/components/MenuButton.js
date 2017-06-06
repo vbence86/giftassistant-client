7ecb31281983a34ae0525bf8ae23b2f7502ba1d7
Bence Varga <vbence86@gmail.com>
2017-06-06 17:23:41 +0100

    polish MenuButton element

--- src/js/components/MenuButton.js
+++ src/js/components/MenuButton.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 const styles = StyleSheet.create({
-  menuButton: {
+  container: {
     width: '100%',
+    height: 50,
     alignItems: 'flex-start',
     marginTop: 0,
-    backgroundColor: '#f2f2f2'
+    backgroundColor: '#f2fff2'
+  },
+  button: {
+    margin: 10
   }
 });
 
@@ -20,14 +25,14 @@ export default class MenuButton extends Component {
 
   render() {
     return (
-      <View style={styles.menuButton} >
-        <TouchableOpacity 
-          onPress={this.handlePress.bind(this)}
-          style={this.props.style}>
-          <Text>{this.props.children}</Text>
-          <Image
-            source={{ uri: 'http://i.imgur.com/vKRaKDX.png', width: 40, height: 40, }} />        
-        </TouchableOpacity>      
+      <View style={styles.container} >
+        <Icon 
+          name="bars"
+          type="font-awesome"
+          size={32}
+          iconStyle={styles.button}
+          onPress={this.handlePress.bind(this)} />
+        <Text>{this.props.children}</Text>
       </View>
     );
   }
