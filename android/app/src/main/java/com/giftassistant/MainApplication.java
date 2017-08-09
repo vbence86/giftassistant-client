8f890c311d4cee3392567d13608526c74715af03
Bence Varga <vbence86@gmail.com>
2017-08-09 15:22:54 +0200

    rebuild podfile

--- android/app/src/main/java/com/giftassistant/MainApplication.java
+++ android/app/src/main/java/com/giftassistant/MainApplication.java
@@ -3,6 +3,10 @@ package com.giftassistant;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.auth0.react.A0Auth0Package;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.horcrux.svg.SvgPackage;
+import com.github.yamill.orientation.OrientationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new A0Auth0Package(),
+            new VectorIconsPackage(),
+            new SvgPackage(),
+            new OrientationPackage()
       );
     }
   };
