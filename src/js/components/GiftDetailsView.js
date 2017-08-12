6869bb8f4202afd82894aef9e7d6b4ee31ac4b30
vbence86 <vbence86@gmail.com>
2017-08-12 15:36:51 +0200

    restyle GiftDetailsView

--- src/js/components/GiftDetailsView.js
+++ src/js/components/GiftDetailsView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, Animated } from 'react-native';
-import { Grid, Row, Column, Button, Text } from 'react-native-elements';
+import { StyleSheet, View, Image, Animated, ScrollView } from 'react-native';
+import { Grid, Row, Column, Button, Text, Divider } from 'react-native-elements';
 import StarRating from 'react-native-star-rating';
 import BackButton from './BackButton';
 
 const FONT_SIZE_DEFAULT = 30;
 const FONT_SIZE_SMALL = 20;
+const FONT_SIZE_BUTTON = 12;
 
 const styles = StyleSheet.create({
   navigatorContainer: {
@@ -23,67 +24,73 @@ const styles = StyleSheet.create({
     justifyContent: 'center'    
   },  
   ctaContainer: {
-    width: '100%',
-    height: '30%',
-    flex: 1,
-    flexWrap: 'wrap',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'     
   },
-  buttonSmall: {
+  button: {
+    width: '40%',
+    height: 50,
+    backgroundColor: '#007aff',
+    borderRadius: 10,
+  },
+  buttonInverse: {
     width: '40%',
     height: '50%',
     margin: '5%',
-    backgroundColor: '#397af8',
-    borderRadius: 5,
-  },  
+    backgroundColor: 'red',
+    borderRadius: 10,
+  },
   contentContainer: {
     width: '100%',
-    height: '70%',
-    flexDirection: 'row'
+    height: '150%',
   },
   imageContainer: {
-    width: '48%',
-    height: '60%',
-    marginLeft: '2%',
-    marginTop: '2%',
+    marginTop: 10,
+    marginLeft: '25%',
+    marginRight: '25%',
+    maxHeight: 200,
   },
   textContainer: {
-    width: '46%',
-    height: '80%',
     marginLeft: '2%',
     marginTop: '2%',
     marginRight: '2%',
     marginBottom: '2%',
   },
   textHeader: {
+    color: '#333',
     fontSize: 18,
-    textAlign: 'justify'
+    textAlign: 'center',
   },
   textDescription: {
-    marginTop: 20,
+    color: '#333',    
+    marginTop: 10,
     fontSize: 13,
     textAlign: 'justify',
-    color: '#333'
+    lineHeight: 20,
   },  
   textRatingContainer: {
-    width: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
     marginTop: 10,
-  },  
+  },
+  textRatingCount: {
+    paddingLeft: 5,
+    fontSize: 9,
+    color: '#aaa',
+  },
   textPriceContainer: {
     width: '100%',
     marginTop: 20,
     alignItems: 'center',
     justifyContent: 'space-between'
   },
-  textPrice: {
-    color: 'red',
-    fontSize: 25,
-  },
   image: {
     width: '100%',
-    minHeight: '100%',
+    height: '100%',
+    maxHeight: 300,
   },
   priceLabelText: {
     width: '100%',
@@ -98,7 +105,10 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontWeight: 'bold',
     fontSize: 16,
-  },  
+  },
+  divider: {
+    backgroundColor: '#aaa',
+  }  
 });
 
 export default class GiftDetailsView extends React.Component {
@@ -120,7 +130,7 @@ export default class GiftDetailsView extends React.Component {
           <BackButton onPress={this.props.onBack} />
           <Text style={styles.MenuHeaderText}>{this.data.label}</Text>
         </View>
-        <View style={styles.contentContainer}>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
           <View style={styles.imageContainer}>
             <Image style={styles.image} source={{uri: this.data.largeImageURL}} />
           </View>
@@ -136,21 +146,20 @@ export default class GiftDetailsView extends React.Component {
                 maxStars={5}
                 rating={4.5}
                 starColor={'#ffc200'}
-                starSize={20}
+                starSize={10}
               />
+              <Text style={styles.textRatingCount}>(38)</Text>
             </View>
-            <View style={styles.textPriceContainer}>
-              <Text style={styles.textPrice}>{this.data.formattedPrice}</Text>
-            </View>
+            <View style={styles.ctaContainer}>
+              <Button fontSize={FONT_SIZE_BUTTON} fontWeight='bold' buttonStyle={styles.button} title={`Buy for ${this.data.formattedPrice} on Amazon`} onPress={this.props.onBuy} large/>
+              <Button fontSize={FONT_SIZE_BUTTON} buttonStyle={styles.buttonInverse} title="Remove" onPress={this.props.onRemove} large/>
+            </View> 
+            <Divider style={styles.divider} />           
             <View style={styles.textDescriptionContanier}>
               <Text style={styles.textDescription}>{this.data.description}</Text>
             </View>
           </View>
-        </View>
-        <View style={styles.ctaContainer}>
-          <Button fontSize={FONT_SIZE_SMALL} buttonStyle={styles.buttonSmall} title="Remove" onPress={this.props.onRemove} raised large/>
-          <Button fontSize={FONT_SIZE_SMALL} buttonStyle={styles.buttonSmall} title="Buy" onPress={this.props.onBuy} raised large/>
-        </View>
+        </ScrollView>
       </View>
     );
   }
